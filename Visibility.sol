4afc5d7a7ee996da011ccbaf03baac9a225b0a43
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-09-13 21:57:12 -0400

    Update Visibility.sol

--- Visibility.sol
+++ Visibility.sol
@@ -3,15 +3,28 @@ pragma solidity ^0.6.0;
 contract VisibilityExamples {
 
     uint256 public publicNumber;
+    uint256 private privateNumber;
     string externalString;
+    bool internal internalBool;
+
+    //Can only be called from withing this contract and any derived contracts
+    function storeInternalBool(bool _internalBool) internal {
+        internalBool = _internalBool;
+    }
 
     //All access function but can be costly since all arguments are written to memory 
     function storePublicNumber(uint _publicNumber) public {
+        storeInternalBool(_publicNumber < 1);
         publicNumber = _publicNumber;
     }
 
     //Cannot be accessed internally, only externally. Cheaper since arguments are stored in calldata
     function storeExternalString(string calldata _externalString) external {
         externalString = _externalString;
+    }
+
+    //Can only be called by the main contract
+    function storePrivateNumber(uint256 _privateNumber) private {
+        privateNumber = _privateNumber;
     }
 }
