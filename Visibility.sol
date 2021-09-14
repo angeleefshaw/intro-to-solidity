a15cf3aa9738e3c90837f8cbbacda5afb456f8c3
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-09-13 22:59:34 -0400

    Update Visibility.sol
    view and pure functions

--- Visibility.sol
+++ Visibility.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.6.0;
 
 contract VisibilityExamples {
 
+    //if no visibility is set, the visibility is automatically set to internal
     uint256 public publicNumber;
     uint256 private privateNumber;
     string externalString;
@@ -27,4 +28,16 @@ contract VisibilityExamples {
     function storePrivateNumber(uint256 _privateNumber) private {
         privateNumber = _privateNumber;
     }
+
+    //view functions used to simply read off the blockchain without changing any variables state.  
+    function retrieve() public view returns(uint256) {
+        return publicNumber;
+    }
+
+    //pure functions ensure no state will be read or changed 
+    function retrievePurely(uint256 favoriteNumber, uint256 i, uint256 j) public pure returns(uint256) {
+        //notice no state is saved anywhere
+        favoriteNumber * 10;
+        return i + j;
+    } 
 }
