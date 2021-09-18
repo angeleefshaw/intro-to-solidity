f7b53b66915c7bdf1020482e52aad90e68596ab2
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-09-18 11:51:12 -0400

    Update DataStorage.sol

--- DataStorage.sol
+++ DataStorage.sol
@@ -13,10 +13,14 @@ contract DataStorage {
     //dynamic array that can hold a list of people
     People[] public people;
 
-    //There are two ways to store information - memory and storage. Data stored in memory is session based while data stored in storage is persistent. 
+    //mapping
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    //There are two ways to store information - memory and storage. Data stored in memory is execution based while data stored in storage is persistent. 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         //create a people object and push it into people array
         people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 
 }
